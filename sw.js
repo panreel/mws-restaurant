7b1c709e5d76dede94d1ec153494216cf5005b8e
Nico Cinefra <ncinefra@gmail.com>
2018-04-06 02:34:46 +0200

    ServiceWorker added!

--- sw.js
+++ sw.js
@@ -1,12 +1,34 @@
+var restaurantsCacheName = 'restaurantsapp-cache-v1';
+
+//SW install
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('v1').then(function(cache) {
+        caches.open(restaurantsCacheName).then(function(cache) {
           return cache.addAll([
-            '/index.html',
+            '/',
+            '/js/dbhelper.js',
             '/js/main.js',
+            '/js/restaurant_info.js',
+            '/css/styles.css',
+            '/css/styles-medium.css',
+            '/css/styles-large.css',
             '/data/restaurants.json',
-            '/img/'
+            '/restaurant.html',
           ]);
         })
-      );
-})
+    );
+});
+
+//SW fetch
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return (!event.request.url.endsWith('.jpg'))? response : caches.open(restaurantsCacheName).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
+      })
+    })
+  )
+});
